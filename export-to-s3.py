38a23116a34c724aa2ad2c97b0cb28f74b82d6e1
Sakib Islam <152702722+sakib090@users.noreply.github.com>
2026-06-05 16:07:29 +0100

    Add files via upload

---
+++ export-to-s3.py
@@ -0,0 +1,34 @@
+import json
+import boto3
+from pymongo import MongoClient
+from bson import json_util
+
+ 
+# Connect to MongoDB
+client = MongoClient("mongodb://localhost:27017/")
+db = client["weather_db"]
+collection = db["London_Forecast"]
+
+# Fetch all documents from MongoDB (including _id)
+documents = list(collection.find())
+
+# Export to JSON file
+with open("weather_data.json", "w") as f:
+    json.dump(documents, f, default=json_util.default, indent=4)
+print("Data exported to weather_data.json")
+
+ 
+# Upload to S3
+s3_client = boto3.client('s3', region_name='eu-central-1')
+
+ 
+s3_client.upload_file(
+    Filename="weather_data.json",
+    Bucket="se-data-with-ai-etl-project",
+    Key="********/weather_data.json"
+)
+
+print("Uploaded to S3")
+
+
+
