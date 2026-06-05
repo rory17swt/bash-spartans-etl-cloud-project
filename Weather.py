29e2abd64ef7aba7f75ea289b6ad828445d1f257
Cracked005 <w1893593@my.westminster.ac.uk>
2026-06-05 16:17:48 +0100

    added weather ETL pipeline

--- Weather.py
+++ Weather.py
@@ -1,8 +1,6 @@
 import requests
 import pprint as pp
 
-
-# List of cities with coordinates
 cities = [
     {"name": "London", "lat": 51.5, "lon": -0.12},
     {"name": "Paris", "lat": 48.85, "lon": 2.35},
@@ -11,16 +9,11 @@ cities = [
 ]
 
 for city in cities:
-    # Build the API URL iterating thorugh each city and coordinates
     url = f"https://api.open-meteo.com/v1/forecast?latitude={city['lat']}&longitude={city['lon']}&hourly=temperature_2m,relative_humidity_2m,wind_speed_10m&current=temperature_2m,wind_speed_10m&timezone=Europe%2FLondon"
     
-    # Fetch the weather data from the API
     response = requests.get(url)
-
-    # Parse the JSON response into a Python dictionary
     data = response.json()
-
-    # Add the city name to the data (The API response doesn't include it)
     data['city'] = city['name']
     
-    print("successful")
+    print(f"\n--- {city['name']} ---")
+    pp.pprint(data['current'])
--- Weather_Mangodb.py
+++ Weather_Mangodb.py
@@ -3,12 +3,10 @@ from pymongo import MongoClient
 import pprint as pp 
 
 
-# Connect to local MongoDB and target the weather database and collection
 client = MongoClient()
 db = client ["weather_db"]
 collection = db["London_Forecast"]
 
-# List of cities with their coordinates
 cities = [
     {"name": "London", "lat": 51.5, "lon": -0.12},
     {"name": "Paris", "lat": 48.85, "lon": 2.35},
@@ -16,24 +14,15 @@ cities = [
     {"name": "Madrid", "lat": 40.42, "lon": -3.70}
 ]
 
-# List to hold all city weather documents before inserting
 records = []
 
 for city in cities:
-
-    # Build the API URL iterating through each city and coordinates
     url = f"https://api.open-meteo.com/v1/forecast?latitude={city['lat']}&longitude={city['lon']}&hourly=temperature_2m,relative_humidity_2m,wind_speed_10m&current=temperature_2m,wind_speed_10m&timezone=Europe%2FLondon"
     
-    # Fetched weather data from API
     response = requests.get(url)
     data = response.json()
-
-    # Add the city name to the data (the API doesn't include it)
     data['city'] = city['name']
-
-    # Append the document to the records list (builds up all city data before bulk insert)
     records.append(data)
     print(f"Fetched {city['name']}")
 
-# Insert all the city documents into MongoDB 
 collection.insert_many(records)
